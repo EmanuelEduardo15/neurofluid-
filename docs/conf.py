45ecd67b95345af0db0565a482721092023da8e4
Emanuel Eduardo <emapica15@gmail.com>
2025-03-25 15:45:15 +0000

    Create conf.py
    Documentação Automatizada

---
+++ docs/conf.py
@@ -0,0 +1,11 @@
+project = 'Neurofluid'
+author = 'Seu Nome'
+release = '0.1.0'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode'
+]
+
+html_theme = 'furo'
